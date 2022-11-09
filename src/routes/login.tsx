e6a645ce52590dc7c06815857beb20d611120db5
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-09 19:32:48 +0200

    Log user out everytime user enters /login

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Alert from '../components/alert';
 import LoginDialog from '../components/login/LoginDialog';
+import useAuth from '../hooks/useAuth';
 
-const Login = () => (
-  <>
-    <Alert />
-    <LoginDialog />
-  </>
-);
+const Login = () => {
+  const auth = useAuth();
+
+  useEffect(() => {
+    auth.logout();
+  }, []);
+
+  return (
+    <>
+      <Alert />
+      <LoginDialog />
+    </>
+  );
+};
 
 export default Login;
