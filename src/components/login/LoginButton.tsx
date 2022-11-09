a67ce3b43306a2339c9293e5806e6df02dcf0565
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-09 20:03:46 +0200

    Increase font size

--- src/components/login/LoginButton.tsx
+++ src/components/login/LoginButton.tsx
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 
 const StyledLoginButton = styled(Button)(({ theme }) => ({
+  fontSize: '1.1rem',
   fontWeight: 'bold',
   height: '60px',
   backgroundColor: theme.button.main,
