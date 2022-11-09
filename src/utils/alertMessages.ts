a704c40fc000e2172828b6fc868e68d74798eb1f
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-09 19:32:30 +0200

    Add logout alert messages

--- src/utils/alertMessages.ts
+++ src/utils/alertMessages.ts
@@ -1,4 +1,6 @@
 // Error
 export const LOGIN_FAILED = 'Failed to login. Check your email and password.';
+export const LOGOUT_FAILED = 'Logging out failed, try again later.';
 // Success
 export const LOGIN_SUCCESS = 'Logged in.';
+export const LOGOUT_SUCCESS = 'Logged out.';
