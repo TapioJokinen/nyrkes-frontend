df70c7d3b409284ed6d26c66659bf7039451f478
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-10 21:30:56 +0200

    Add constants.ts

---
+++ src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const NAV_BAR_PAGES: string[] = ['Page1', 'Page2', 'Page3'];
+export const NAV_BAR_SETTINGS: string[] = ['Profile', 'Account', 'Dashboard', 'Logout'];
+export const NAV_BAR_TITLE = 'Nyrkes';
