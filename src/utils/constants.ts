defc609851ccd07a1500a3c33e294099d7698898
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-13 17:54:05 +0200

    Change NAV_BAR_PAGES list elements

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,6 @@
-export const NAV_BAR_PAGES: string[] = ['Page1', 'Page2', 'Page3'];
-export const NAV_BAR_SETTINGS: string[] = ['Profile', 'Account', 'Dashboard', 'Logout'];
+export const NAV_BAR_PAGES = [
+  { name: 'Home', path: '/' },
+  { name: 'Dashboard', path: '/dashboard' },
+];
+export const NAV_BAR_SETTINGS: string[] = ['Profile', 'Settings'];
 export const NAV_BAR_TITLE = 'Nyrkes';
