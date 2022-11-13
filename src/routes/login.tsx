375c04497cf57b70bae12b271952d4cea8f6ca09
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-13 17:53:13 +0200

    Add navigate() to auth.logout() callback function

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,14 +1,19 @@
 import React, { useEffect } from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
 import Alert from '../components/alert';
 import LoginDialog from '../components/login';
 import useAuth from '../hooks/useAuth';
 
 const Login = () => {
   const auth = useAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    auth.logout();
+    auth.logout(() => {
+      navigate('/login', { replace: true });
+    });
   }, []);
 
   return (
