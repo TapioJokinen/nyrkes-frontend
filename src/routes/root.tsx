0a5c991d12ea5cd63378984fe8bc09202c666fbd
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-13 17:53:23 +0200

    Add Alert component

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { Outlet } from 'react-router-dom';
 
+import Alert from '../components/alert';
 import NavBar from '../components/navbar';
 
 const Root = () => (
   <>
+    <Alert />
     <NavBar />
     <Outlet />
   </>
