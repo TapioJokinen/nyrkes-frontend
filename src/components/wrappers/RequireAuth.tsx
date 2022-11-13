68bd9e743a894957e5e988c00e8060e5ff627cab
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-13 17:51:23 +0200

    Fix logout redirect

--- src/components/wrappers/RequireAuth.tsx
+++ src/components/wrappers/RequireAuth.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { useLocation, Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { verifyToken } from '../../api/auth';
 import useAuth from '../../hooks/useAuth';
@@ -8,12 +8,13 @@ import useFetch from '../../hooks/useFetch';
 
 const RequireAuth = ({ children }: { children: JSX.Element }) => {
   const auth = useAuth();
-  const location = useLocation();
+  const navigate = useNavigate();
   const response = useFetch(verifyToken, []);
 
   if (response.error) {
-    auth.logout();
-    return <Navigate to="/login" state={{ from: location }} replace />;
+    auth.logout(() => {
+      navigate('/login', { replace: true });
+    });
   }
 
   if (response.ok) {
